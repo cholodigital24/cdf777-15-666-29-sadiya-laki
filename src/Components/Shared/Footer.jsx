b5fb0164ba61a5bb3a5d4333d501583f9b020211
m-sabid <sabidofficial@gmail.com>
2024-01-30 23:18:44 +0600

    added and update amp and contact and index

--- src/Components/Shared/Footer.jsx
+++ src/Components/Shared/Footer.jsx
@@ -5,8 +5,8 @@ function Footer() {
     <footer className="bg-dark text-white py-4 mt-8">
       <div className="container mx-auto flex flex-col md:flex-row items-center justify-between px-4">
         <div className="mb-4 md:mb-0">
-          <h2 className="text-2xl font-bold">Bidya Sinha Saha Mim</h2>
-          <p className="text-gray-300">Actress | Tollywood.</p>
+          <h2 className="text-2xl font-bold">সাদিয়া লাকী</h2>
+          <p className="text-gray-300">SK | BRAC.</p>
         </div>
         <div className="flex items-center space-x-4">
           <a
@@ -38,8 +38,8 @@ function Footer() {
       <div className="mt-8 mx-4 flex justify-center items-center">
         <p className="text-gray-500 text-sm">
           &copy; 2023 Bio Site. All rights reserved. || Created by{" "}
-          <a href="tel:+8801985381288" className="text-white">
-            Hasibul Islam
+          <a href="tel:+881826311125" className="text-white">
+            Cholo Digital
           </a>
         </p>
       </div>
