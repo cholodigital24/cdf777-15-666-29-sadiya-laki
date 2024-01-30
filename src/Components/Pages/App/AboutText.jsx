50da38e756eccf7510ebdb00da18dca46c282c30
m-sabid <sabidofficial@gmail.com>
2024-01-30 23:50:34 +0600

    change name

--- src/Components/Pages/App/AboutText.jsx
+++ src/Components/Pages/App/AboutText.jsx
@@ -8,7 +8,7 @@ function AboutText() {
         <div>
           <div className="grid gap-4">
             <p>
-              Sadiya Laki radiates resilience and positivity in all she does. As
+              Sadia Laki radiates resilience and positivity in all she does. As
               a self-defending enthusiast, she embodies confidence and a
               proactive mindset, tackling challenges with determination. Her
               diverse skill set includes roles such as Sahsthokormi Brack and
@@ -17,7 +17,7 @@ function AboutText() {
             </p>
 
             <p>
-              Beyond her professional endeavors, Sadiya is deeply committed to
+              Beyond her professional endeavors, Sadia is deeply committed to
               community welfare. She actively engages with organizations like
               Sitylioclube of Dhaka Flwalw Garden International and UNICEF
               Bangladesh, leveraging her skills to uplift and empower others. At
@@ -26,7 +26,7 @@ function AboutText() {
             </p>
 
             <p>
-              Sadiya's spirit of service extends even further as she volunteers
+              Sadia's spirit of service extends even further as she volunteers
               as a firefighter at 8no Word Fire Service, bravely standing on the
               front lines to protect her community. Her unwavering dedication
               and passion make her a valuable asset in every role she
